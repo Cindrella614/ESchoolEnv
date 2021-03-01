3417260698884ccc0fb426340eff045cd8fb0121
Segdc-Software <cindrellanithin@gmail.com>
2021-03-01 14:05:42 +0530

    Dynamic URL added

--- ESchoolOffice/staff/urls.py
+++ ESchoolOffice/staff/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^staffdetails/$', staff, name="staffDetails"),
     url(r'^designation/add/$', DesignationCreate.as_view(), name='designation-add'),
     url(r'^designation/$', DesignationList.as_view()),
+    url(r'^designation/(?P<pk>\d+)/edit/$', DesignationUpdate.as_view()),
     url(r'^leavetype/$', leavetype, name="leaveType"),
     url(r'^staffleave/$', staffleave, name="staffLeave"),
     url(r'^teachersubjects/$', teachersubjects, name="teacherSubjects"),
